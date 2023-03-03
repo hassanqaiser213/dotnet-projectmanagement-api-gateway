e93ee924b8a318f95353c50dc471961a03301440
afroze9 <afrozeamjad@gmail.com>
2023-03-03 17:20:35 +0500

    Added ocelot as api gateway

---
+++ src/ProjectManagement.ApiGateway/DelimitedScopesAuthorizer.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Claims;
+using Ocelot.Authorization;
+using Ocelot.Infrastructure.Claims.Parser;
+using Ocelot.Responses;
+
+namespace ProjectManagement.ApiGateway;
+
+public class DelimitedScopesAuthorizer : IScopesAuthorizer
+{
+    private readonly IClaimsParser _claimsParser;
+    private readonly string _scope = "scope";
+    private readonly string _msScope = "http://schemas.microsoft.com/identity/claims/scope";
+    
+    public DelimitedScopesAuthorizer(IClaimsParser claimsParser)
+    {
+        _claimsParser = claimsParser;
+    }
+    
+    public Response<bool> Authorize(ClaimsPrincipal claimsPrincipal, List<string> routeAllowedScopes)
+    {
+        if (routeAllowedScopes == null || routeAllowedScopes.Count == 0)
+        {
+            return new OkResponse<bool>(true);
+        }
+
+        var values = _claimsParser.GetValuesByClaimType(claimsPrincipal.Claims, _msScope);
+        if (!values.IsError && !values.Data.Any())
+        {
+            values = _claimsParser.GetValuesByClaimType(claimsPrincipal.Claims, _scope);
+        }
+
+        if (values.IsError)
+        {
+            return new ErrorResponse<bool>(values.Errors);
+        }
+
+        List<string> userScopes = new List<string>();
+        foreach (var item in values.Data)
+        {
+            if (item.Contains(' '))
+            {
+                var scopes = item.Split(' ').ToList();
+                userScopes.AddRange(scopes);
+            }
+            else
+            {
+                userScopes.Add(item);
+            }
+        }
+
+        var matchesScopes = routeAllowedScopes.Intersect(userScopes).ToList();
+
+        if (matchesScopes.Count == 0)
+        {
+            return new ErrorResponse<bool>(
+                new ScopeNotAuthorizedError($"no one user scope: '{string.Join(",", userScopes)}' match with some allowed scope: '{string.Join(",", routeAllowedScopes)}'"));
+        }
+
+        return new OkResponse<bool>(true);
+    }
+}
--- src/ProjectManagement.ApiGateway/Program.cs
+++ src/ProjectManagement.ApiGateway/Program.cs
@@ -1,35 +1,53 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Ocelot.Authorization;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+
 namespace ProjectManagement.ApiGateway;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.Configuration.AddJsonFile("ocelot.json");
 
-        // Add services to the container.
+        builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer("Auth0", options =>
+            {
+                options.Authority = "https://afrozeprojectmanagement.us.auth0.com/";
+                options.Audience = "company";
+            });
+        
+        builder.Services
+            .AddOcelot()
+            .AddConsul();
+        
+        builder.Services.CustomizeOcelot();
 
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
+        WebApplication app = builder.Build();
+        app.UseAuthentication();
+        app.UseOcelot().Wait();
 
         app.Run();
+    }
+
+    
+}
+
+// Github Issue: https://github.com/ThreeMammals/Ocelot/issues/913
+internal static class DependencyInjectionExtensions
+{
+    internal static void CustomizeOcelot(this IServiceCollection services)
+    {
+        services.RemoveAll<IScopesAuthorizer>();
+        services.TryAddSingleton<IScopesAuthorizer, DelimitedScopesAuthorizer>();
     }
 }
